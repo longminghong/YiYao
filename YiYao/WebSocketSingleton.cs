5d986db4836613b2b2be47467289945d5b22313c
longminghong <longminghong@LIVE.CN>
2017-04-20 14:04:58 +0800

    添加2.0的界面 添加单例 添加2.0用到的图片

---
+++ YiYao/WebSocketSingleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YiYao
+{
+    class WebSocketSingleton
+    {
+        private static WebSocketSingleton instance;
+        private static object _lock = new object();
+
+        private WebSocketSingleton()
+        {
+
+        }
+        public static WebSocketSingleton GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (_lock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new WebSocketSingleton();
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+
+}
+}
