a6851a995fc8d66d4800920a9fa1c8e8264af307
longminghong <longminghong@gmail.com>
2017-05-11 10:21:38 +0800

    网络请求部分的添加内容
    发送SSN

---
+++ YiYao/V2.0/HTTP.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace YiYao
+{
+    public class HTTP
+    {
+        String ssn;
+        String store_id;
+        private void HttpPost(String SSNNumber,String Store_ID)
+        {   
+            if (SSNNumber== null)
+            {
+                return;
+            }
+            else
+            {
+                ssn = SSNNumber;
+                store_id = Store_ID;
+            }
+            Uri uri = new Uri("http://localhost:3881/Financial.ashx");
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(uri);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+
+            // 
+
+            request.BeginGetRequestStream(new AsyncCallback(RequestProceed), request);
+        }
+        private void RequestProceed(IAsyncResult asyncResult)
+        {   
+            HttpWebRequest request = (HttpWebRequest)asyncResult.AsyncState;
+            StreamWriter postDataWriter = new StreamWriter(request.EndGetRequestStream(asyncResult));
+            postDataWriter.Write("ticker=NTES");
+            postDataWriter.Write("&startdate=1-1-2009");
+            
+            postDataWriter.Close();
+            request.BeginGetResponse(new AsyncCallback(ResponesProceed), request);
+        }
+        
+        private void ResponesProceed(IAsyncResult asyncResult)
+        {
+            WebRequest request = (HttpWebRequest)asyncResult.AsyncState;
+            HttpWebResponse response = (HttpWebResponse)request.EndGetResponse(asyncResult);
+            StreamReader responseReader = new StreamReader(response.GetResponseStream());
+            string responseString = responseReader.ReadToEnd();
+            
+            //Dispatcher.BeginInvoke(() => TimeSeries.ItemsSource = myTimeSeries);
+        }
+
+    }
+}
