f6696186a6ba57850281056cfd0957e0700327af
longminghong <longminghong@gmail.com>
2017-05-12 18:11:45 +0800

    A8页面调整

--- YiYao/V2.0/A8.xaml.cs
+++ YiYao/V2.0/A8.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WebService;
+using Microsoft.Practices.ServiceLocation;
+using Prism.Events;
+using YiYao.Events;
 
 namespace YiYao
 {
@@ -20,24 +24,116 @@ namespace YiYao
     /// </summary>
     public partial class A8 : UserControl, INavigable
     {
+        MTMIssueCollectDTO reciveDTO;
+
         public A8()
         {
             InitializeComponent();
+            baojian.ItemsSource = new string[] { "/YiYao;component/1.png", "/YiYao;component/2.png", "/YiYao;component/3.png", "/YiYao;component/4.png", "/YiYao;component/1.png" };
+            baojian.ManipulationBoundaryFeedback += (s, e) =>
+            {
+                e.Handled = true;
+            };
+
+            yaoscrollviewer.ManipulationBoundaryFeedback += Yaoscrollviewer_ManipulationBoundaryFeedback;
+
+        }
+
+        private void Yaoscrollviewer_ManipulationBoundaryFeedback(object sender, ManipulationBoundaryFeedbackEventArgs e)
+        {
+            e.Handled = true;
         }
 
         public void Start(object args)
         {
+            if (null != args)
+            {
+                reciveDTO = (MTMIssueCollectDTO)args;
 
+                EventAggregator eventAggragator = ServiceLocator.Current.GetInstance<EventAggregator>();
+                eventAggragator.GetEvent<WebSocketEvent>().Subscribe(OnWebSocketEvent);
+            }
         }
 
         public void Stop()
         {
-
+            EventAggregator eventAggragator = ServiceLocator.Current.GetInstance<EventAggregator>();
+            eventAggragator.GetEvent<WebSocketEvent>().Unsubscribe(OnWebSocketEvent);
         }
+        private void OnWebSocketEvent(object data)
+        {   
+            Console.WriteLine("data A8" +
+                "" +
+                " ======== OK ");
 
+            if (0 == reciveDTO.risklevel) return;
+
+            reciveDTO = (MTMIssueCollectDTO)data;
+
+            Uri riskImageUri = null;
+            String ristValueString = "";
+            String riskImagePath = "";
+
+            switch (reciveDTO.risklevel)
+            {
+                case 1:
+                    {
+                        riskImagePath= "pack://application:,,,/人体绿色.png";
+                        ristValueString = "健康";
+                    }
+                    break;
+                case 2:
+                    {
+                        riskImagePath = "pack://application:,,,/人体黄色.png";
+                        
+                        ristValueString = "中危";
+                    }
+                    break;
+                case 3:
+                    {
+                        riskImagePath = "pack://application:,,,/人体红色.png"; 
+
+                        ristValueString = "高危";
+                    }
+
+                    break;
+            }
+ 
+            
+            try
+            {
+                bool imageExist;
+                imageExist = System.IO.File.Exists(riskImagePath);
+
+                riskImageUri = new Uri(riskImagePath);
+                textblock_risk.Text = ristValueString;
+
+                BitmapImage bi3 = new BitmapImage();
+                bi3.BeginInit();
+                bi3.UriSource = riskImageUri;
+                bi3.EndInit();
+                image_risk.Source = bi3;
+
+                textblock_heigh.Text = reciveDTO.measuredata.height.ToString();
+                textblock_weigh.Text = reciveDTO.measuredata.weight.ToString();
+                textblock_bmi.Text = reciveDTO.measuredata.BMI;
+                textblock_waist.Text = reciveDTO.measuredata.waist.ToString();
+            }
+            catch (Exception)
+            {
+                
+            }
+            
+            //int w = riskImage.PixelHeight;
+        }
         private void jiantou1_png_MouseDown(object sender, MouseButtonEventArgs e)
         {
             (Parent as NavigationManager).GoToPage(typeof(ShoppingCar));
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            (Parent as NavigationManager).GoToPage(typeof(Dashboard));
         }
     }
 }
