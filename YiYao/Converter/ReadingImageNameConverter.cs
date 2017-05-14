28eaf193b023749d74a78cb3b2b5c8e6d9e86bdb
longminghong <longminghong@gmail.com>
2017-05-14 15:08:43 +0800

    转换用的coverter

---
+++ YiYao/Converter/ReadingImageNameConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace YiYao
+{
+    public class ReadingImageNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string name = value as string;
+            int start = name.IndexOf(".");
+            int end = name.IndexOf(".png");
+            return name.Substring(start + 1, end - start - 1);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
