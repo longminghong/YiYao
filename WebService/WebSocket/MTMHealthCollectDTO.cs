18e7bc8a2a49ce585c692384c3e34f4c5f5f598f
longminghong <longminghong@gmail.com>
2017-06-26 21:13:13 +0800

    服务器数据变更修正

--- WebService/WebSocket/MTMHealthCollectDTO.cs
+++ WebService/WebSocket/MTMHealthCollectDTO.cs
@@ -11,12 +11,26 @@ namespace WebService
         public int eatingpreference { get; set; }
         public string smoking { get; set; }
         public string drinking { get; set; }
-        public double height { get; set; }
-        public double weight { get; set; }
-        public double waist { get; set; }
-        public int heartrate { get; set; }
-        public double systolicpressure { get; set; }
-        public double diastolicpressure { get; set; }
+        //public double height { get; set; }
+        //public double weight { get; set; }
+        //public double waist { get; set; }
+        //public int heartrate { get; set; }
+        //public double systolicpressure { get; set; }
+        //public double diastolicpressure { get; set; }
+        //public float fastsugar { get; set; }
+        //public float randomsugar { get; set; }
+        //public float HbA1c { get; set; }
+        //public float cholesterol { get; set; }
+        //public float triglyceride { get; set; }
+        //public float ldl { get; set; }
+        //public float hdl { get; set; }
+
+        public float height { get; set; }
+        public float weight { get; set; }
+        public float waist { get; set; }
+        public float heartrate { get; set; }
+        public float systolicpressure { get; set; }
+        public float diastolicpressure { get; set; }
         public float fastsugar { get; set; }
         public float randomsugar { get; set; }
         public float HbA1c { get; set; }
@@ -25,5 +39,11 @@ namespace WebService
         public float ldl { get; set; }
         public float hdl { get; set; }
 
+        public float agobloodfat_tcmaxvalue { get; set; }
+        public float agobloodfat_ldlmaxvalue { get; set; }
+        public float agobloodfat_hdlminvalue { get; set; }
+        public float agobloodfat_tgmaxvalue { get; set; }
     }
+
+     
 }
--- WebService/WebSocket/MTMIssueCollectDTO.cs
+++ WebService/WebSocket/MTMIssueCollectDTO.cs
@@ -25,26 +25,19 @@ namespace WebService
         public Controllable_Risk controllable_risk { get; set; }
         public Measuredata measuredata { get; set; }
         public Chart_Data chart_data { get; set; }
-        public Nowdrug[] nowdrugs { get; set; }
+        public object[] nowdrugs { get; set; }
         public string operatetype { get; set; }
     }
 
- 
-
+  
     public class Icd10
     {
         public string[] _1 { get; set; }
         public string[] _2 { get; set; }
-        public string[] _3 { get; set; }
         public string[] _4 { get; set; }
-        public string[] _6 { get; set; }
         public string[] _7 { get; set; }
-        public string[] _8 { get; set; }
         public string[] _9 { get; set; }
-        public string[] _10 { get; set; }
-        public string[] _11 { get; set; }
         public string[] _12 { get; set; }
-        public string[] _13 { get; set; }
         public string[] _15 { get; set; }
     }
 
@@ -278,12 +271,12 @@ namespace WebService
 
     public class Chinadisease
     {
-        public string[] brain { get; set; }
-        public string[] eye { get; set; }
+        public object[] brain { get; set; }
+        public object[] eye { get; set; }
         public string[] heart { get; set; }
         public string[] blood { get; set; }
         public string[] pancreas { get; set; }
-        public string[] kidney { get; set; }
+        public object[] kidney { get; set; }
     }
 
     public class Controllable_Risk
@@ -298,10 +291,10 @@ namespace WebService
 
     public class Measuredata
     {
-        public double height { get; set; }
-        public double weight { get; set; }
+        public int height { get; set; }
+        public int weight { get; set; }
         public string BMI { get; set; }
-        public double waist { get; set; }
+        public int waist { get; set; }
     }
 
     public class Chart_Data
@@ -315,61 +308,377 @@ namespace WebService
     {
         public string unit { get; set; }
         public string[] x { get; set; }
-        public double[] systolicpressure { get; set; }
-        public double[] diastolicpressure { get; set; }
+        public string[][] systolicpressure { get; set; }
+        public string[][] diastolicpressure { get; set; }
+        public string diamax { get; set; }
+        public string sysmax { get; set; }
     }
 
     public class Xuetang
     {
         public string unit { get; set; }
         public string[] x { get; set; }
-        public double[] fastBloodSugar { get; set; }
-        public double[] randomBloodSugar { get; set; }
+        public string[][] fastBloodSugar { get; set; }
+        public string[][] randomBloodSugar { get; set; }
+        public string fastmax { get; set; }
+        public string randmax { get; set; }
     }
 
     public class Xuezhi
     {
         public string unit { get; set; }
         public string[] x { get; set; }
-        public double[] cholesteroly { get; set; }
-        public double[] triglyceridey { get; set; }
-        public double[] ldlcy { get; set; }
-        public double[] hdlcy { get; set; }
+        public string[][] cholesteroly { get; set; }
+        public string[][] triglyceridey { get; set; }
+        public string[][] ldlcy { get; set; }
+        public string[][] hdlcy { get; set; }
+        public string cholmax { get; set; }
+        public string trigmax { get; set; }
+        public string ldlcmax { get; set; }
+        public string hdlcmax { get; set; }
     }
 
-    public class Nowdrug
-    {
-        public string did { get; set; }
-        public string mid { get; set; }
-        public string sid { get; set; }
-        public string start { get; set; }
-        public string end { get; set; }
-        public string creator { get; set; }
-        public long timestamp { get; set; }
-        public string from { get; set; }
-        public string instructions { get; set; }
-        public string regular { get; set; }
-        public string period { get; set; }
-        public string commonname { get; set; }
-        public string name { get; set; }
-        public string drug_code { get; set; }
-        public string[] drugstype { get; set; }
-        public double __v { get; set; }
-        public Allergic allergic { get; set; }
-        public Method method { get; set; }
-    }
 
-    public class Allergic
-    {
-        public string behavior { get; set; }
-        public string whether { get; set; }
-    }
 
-    public class Method
-    {
-        public string unit { get; set; }
-        public int day { get; set; }
-    }
+    //public class Icd10
+    //{
+    //    public string[] _1 { get; set; }
+    //    public string[] _2 { get; set; }
+    //    public string[] _3 { get; set; }
+    //    public string[] _4 { get; set; }
+    //    public string[] _6 { get; set; }
+    //    public string[] _7 { get; set; }
+    //    public string[] _8 { get; set; }
+    //    public string[] _9 { get; set; }
+    //    public string[] _10 { get; set; }
+    //    public string[] _11 { get; set; }
+    //    public string[] _12 { get; set; }
+    //    public string[] _13 { get; set; }
+    //    public string[] _15 { get; set; }
+    //}
+
+    //public class Code_Correspondence
+    //{
+    //    public _1 _1 { get; set; }
+    //    public _2 _2 { get; set; }
+    //    public _3 _3 { get; set; }
+    //    public _4 _4 { get; set; }
+    //    public _5 _5 { get; set; }
+    //    public _6 _6 { get; set; }
+    //    public _7 _7 { get; set; }
+    //    public _8 _8 { get; set; }
+    //    public _9 _9 { get; set; }
+    //    public _10 _10 { get; set; }
+    //    public _11 _11 { get; set; }
+    //    public _12 _12 { get; set; }
+    //    public _13 _13 { get; set; }
+    //    public _15 _15 { get; set; }
+    //}
+
+    //public class _1
+    //{
+    //    public string name { get; set; }
+    //    public Subdise subdise { get; set; }
+    //}
+
+    //public class Subdise
+    //{
+    //    public string _1 { get; set; }
+    //}
+
+    //public class _2
+    //{
+    //    public string name { get; set; }
+    //    public Subdise1 subdise { get; set; }
+    //}
+
+    //public class Subdise1
+    //{
+    //    public string E78501 { get; set; }
+    //    public string E78001 { get; set; }
+    //    public string E7815178053 { get; set; }
+    //    public string E78203 { get; set; }
+    //    public string E78451 { get; set; }
+    //}
+
+    //public class _3
+    //{
+    //    public string name { get; set; }
+    //    public Subdise2 subdise { get; set; }
+    //}
+
+    //public class Subdise2
+    //{
+    //    public string _3 { get; set; }
+    //}
+
+    //public class _4
+    //{
+    //    public string name { get; set; }
+    //    public Subdise3 subdise { get; set; }
+    //}
+
+    //public class Subdise3
+    //{
+    //    public string I65202 { get; set; }
+    //    public string I65201 { get; set; }
+    //}
+
+    //public class _5
+    //{
+    //    public string name { get; set; }
+    //    public Subdise4 subdise { get; set; }
+    //}
+
+    //public class Subdise4
+    //{
+    //    public string _5 { get; set; }
+    //}
+
+    //public class _6
+    //{
+    //    public string name { get; set; }
+    //    public Subdise5 subdise { get; set; }
+    //}
+
+    //public class Subdise5
+    //{
+    //    public string I64X04 { get; set; }
+    //    public string I69452 { get; set; }
+    //    public string I61902 { get; set; }
+    //    public string I69101 { get; set; }
+    //    public string G45901 { get; set; }
+    //}
+
+    //public class _7
+    //{
+    //    public string name { get; set; }
+    //    public Subdise6 subdise { get; set; }
+    //}
+
+    //public class Subdise6
+    //{
+    //    public string I74306 { get; set; }
+    //    public string E14503 { get; set; }
+    //    public string I73904 { get; set; }
+    //    public string I99X03 { get; set; }
+    //}
+
+    //public class _8
+    //{
+    //    public string name { get; set; }
+    //    public Subdise7 subdise { get; set; }
+    //}
+
+    //public class Subdise7
+    //{
+    //    public string H35952 { get; set; }
+    //    public string H35006 { get; set; }
+    //    public string E14304 { get; set; }
+    //    public string H35001 { get; set; }
+    //}
+
+    //public class _9
+    //{
+    //    public string name { get; set; }
+    //    public Subdise8 subdise { get; set; }
+    //}
+
+    //public class Subdise8
+    //{
+    //    public string I20 { get; set; }
+    //    public string I25551 { get; set; }
+    //    public string I25101 { get; set; }
+    //    public string I25652 { get; set; }
+    //    public string E14551 { get; set; }
+    //    public string I11901 { get; set; }
+    //    public string I11951 { get; set; }
+    //    public string I50 { get; set; }
+    //    public string I11051 { get; set; }
+    //    public string I11052 { get; set; }
+    //}
+
+    //public class _10
+    //{
+    //    public string name { get; set; }
+    //    public Subdise9 subdise { get; set; }
+    //}
+
+    //public class Subdise9
+    //{
+    //    public string N03 { get; set; }
+    //    public string N03952 { get; set; }
+    //    public string N04903 { get; set; }
+    //    public string E14203 { get; set; }
+    //    public string N03903 { get; set; }
+    //    public string I12903 { get; set; }
+    //    public string I12001 { get; set; }
+    //}
+
+    //public class _11
+    //{
+    //    public string name { get; set; }
+    //    public Subdise10 subdise { get; set; }
+    //    public Educationimg educationimg { get; set; }
+    //}
+
+    //public class Subdise10
+    //{
+    //    public string E10E14 { get; set; }
+    //}
+
+    //public class Educationimg
+    //{
+    //    public string[] 血糖监测 { get; set; }
+    //    public string[] 糖尿病食物金字塔 { get; set; }
+    //    public string[] 预防二型糖尿病 { get; set; }
+    //    public string[] 高血压的危险因子 { get; set; }
+    //}
+
+    //public class _12
+    //{
+    //    public string name { get; set; }
+    //    public Subdise11 subdise { get; set; }
+    //}
+
+    //public class Subdise11
+    //{
+    //    public string E14408 { get; set; }
+    //    public string E14503 { get; set; }
+    //    public string E14606 { get; set; }
+    //    public string E14304 { get; set; }
+    //    public string E14551 { get; set; }
+    //    public string E14203 { get; set; }
+    //}
+
+    //public class _13
+    //{
+    //    public string name { get; set; }
+    //    public Subdise12 subdise { get; set; }
+    //}
+
+    //public class Subdise12
+    //{
+    //    public string R73002 { get; set; }
+    //}
+
+    //public class _15
+    //{
+    //    public string name { get; set; }
+    //    public Subdise13 subdise { get; set; }
+    //    public Educationimg1 educationimg { get; set; }
+    //}
+
+    //public class Subdise13
+    //{
+    //    public string I10X02 { get; set; }
+    //    public string I10X11 { get; set; }
+    //    public string I10I15 { get; set; }
+    //}
+
+    //public class Educationimg1
+    //{
+    //    public string[] 防止血压剧烈波动 { get; set; }
+    //    public string[] 高血压的危险因子 { get; set; }
+    //    public string[] 高血压患者的生活保健13 { get; set; }
+    //    public string[] 量血压的注意事项 { get; set; }
+    //    public string[] 预防二型糖尿病 { get; set; }
+    //}
+
+    //public class Chinadisease
+    //{
+    //    public string[] brain { get; set; }
+    //    public string[] eye { get; set; }
+    //    public string[] heart { get; set; }
+    //    public string[] blood { get; set; }
+    //    public string[] pancreas { get; set; }
+    //    public string[] kidney { get; set; }
+    //}
+
+    //public class Controllable_Risk
+    //{
+    //    public string smoking { get; set; }
+    //    public string drinking { get; set; }
+    //    public string fat { get; set; }
+    //    public string eat { get; set; }
+    //    public string usedrug { get; set; }
+    //    public string threehight { get; set; }
+    //}
+
+    //public class Measuredata
+    //{
+    //    public double height { get; set; }
+    //    public double weight { get; set; }
+    //    public string BMI { get; set; }
+    //    public double waist { get; set; }
+    //}
+
+    //public class Chart_Data
+    //{
+    //    public Xueya xueya { get; set; }
+    //    public Xuetang xuetang { get; set; }
+    //    public Xuezhi xuezhi { get; set; }
+    //}
+
+    //public class Xueya
+    //{
+    //    public string unit { get; set; }
+    //    public string[] x { get; set; }
+    //    public double[] systolicpressure { get; set; }
+    //    public double[] diastolicpressure { get; set; }
+    //}
+
+    //public class Xuetang
+    //{
+    //    public string unit { get; set; }
+    //    public string[] x { get; set; }
+    //    public double[] fastBloodSugar { get; set; }
+    //    public double[] randomBloodSugar { get; set; }
+    //}
+
+    //public class Xuezhi
+    //{
+    //    public string unit { get; set; }
+    //    public string[] x { get; set; }
+    //    public double[] cholesteroly { get; set; }
+    //    public double[] triglyceridey { get; set; }
+    //    public double[] ldlcy { get; set; }
+    //    public double[] hdlcy { get; set; }
+    //}
+
+    //public class Nowdrug
+    //{
+    //    public string did { get; set; }
+    //    public string mid { get; set; }
+    //    public string sid { get; set; }
+    //    public string start { get; set; }
+    //    public string end { get; set; }
+    //    public string creator { get; set; }
+    //    public long timestamp { get; set; }
+    //    public string from { get; set; }
+    //    public string instructions { get; set; }
+    //    public string regular { get; set; }
+    //    public string period { get; set; }
+    //    public string commonname { get; set; }
+    //    public string name { get; set; }
+    //    public string drug_code { get; set; }
+    //    public string[] drugstype { get; set; }
+    //    public double __v { get; set; }
+    //    public Allergic allergic { get; set; }
+    //    public Method method { get; set; }
+    //}
+
+    //public class Allergic
+    //{
+    //    public string behavior { get; set; }
+    //    public string whether { get; set; }
+    //}
+
+    //public class Method
+    //{
+    //    public string unit { get; set; }
+    //    public int day { get; set; }
+    //}
 
 
 
