565fcb5330bbb6a0d64dbc46bbb43892115f7096
longminghong <longminghong@gmail.com>
2017-06-29 14:24:28 +0800

    添加log输出入口

--- YiYao/App.xaml.cs
+++ YiYao/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LogService;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -14,13 +15,26 @@ namespace YiYao
     /// </summary>
     public partial class App : Application
     {
+        private static readonly IBizLogger logger = ServerLogFactory.GetLogger(typeof(Application));
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+            
+            Application.Current.DispatcherUnhandledException += Current_DispatcherUnhandledException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
             var bootstrapper = new Bootstrapper();
             bootstrapper.Run();
         }
 
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            logger.Error("CurrentDomain_UnhandledException"　+ e.ExceptionObject.ToString());
+        }
 
+        private void Current_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+            logger.Error("Current_DispatcherUnhandledException", e.Exception);
+        }
     }
 }
